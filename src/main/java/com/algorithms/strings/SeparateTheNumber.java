f61d8f5f47d192fe7077fd42119dbd9f8540b6a4
Usman <azhar.rao@gmail.com>
2017-07-02 16:37:37 -0700

    initial commit

---
+++ src/main/java/com/algorithms/strings/SeparateTheNumber.java
@@ -0,0 +1,16 @@
+package com.algorithms.strings;
+
+import java.util.Scanner;
+
+public class SeparateTheNumber {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int q = in.nextInt();
+		for(int a0=0; a0 < q; a0++) {
+			String s = in.next();
+			// your code goes here
+		}
+	}
+	
+}
